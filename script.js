013c68782717fbdf13fe8ddf5de2bd6542f43209
gskifstad <gskifstad@gmail.com>
2020-10-27 15:00:44 -0500

    foo

---
+++ script.js
@@ -0,0 +1,12 @@
+// const inherited_role_id = 35;
+// const role_id = 22;
+
+try {
+  // resp = civis.api.call("put", `/roles/${role_id}/inherited_roles/${inherited_role_id}`);
+ // resp = civis.api.call("put", "/roles/22/inherited_roles/35");
+  resp = civis.api.call("patch", "/groups/12773", { "defaultJobsLabel": "tech_job" });
+  log("success", resp);
+  // log(`successfully added ${inherited_role_id} as an inherited role for Role#${role_id}`);
+} catch (err) {
+  log(err);
+}
